29fd015397c7e7888078f0a7a5208bad857f0d21
Florian Richter <Florian.Richter@dbfuhrpark.com>
2024-10-27 09:00:28 +0100

    neo4j added index types

---
+++ phi/vectordb/neo4j/index.py
@@ -0,0 +1,14 @@
+# phi/vectordb/neo4j/index.py
+from typing import Dict, Any, Optional
+from pydantic import BaseModel
+
+class Neo4jNativeIndex(BaseModel):
+    pass
+
+class HNSW(BaseModel):
+    m: int = 16
+    ef_construction: int = 200
+    max_connections: int = 16
+    configuration: Dict[str, Any] = {
+        "maintenance_work_mem": "2GB",
+    }
