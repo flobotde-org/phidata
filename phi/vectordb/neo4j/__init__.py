74f80fa1294fa8cc3b6c9ebb001ed6f44a595c81
Florian Richter <Florian.Richter@dbfuhrpark.com>
2024-10-27 09:04:43 +0100

    typo fix

--- phi/vectordb/neo4j/__init__.py
+++ phi/vectordb/neo4j/__init__.py
@@ -1,5 +1,5 @@
 # phi/vectordb/neo4j/__init__.py
 from phi.vectordb.neo4j.neo4j import Neo4jVectorDb
-from phi.vectordb.neo4j.index import Neo4jNativeIndex, Neo4jHNSW
+from phi.vectordb.neo4j.index import Neo4jNativeIndex, HNSW
 from phi.vectordb.neo4j.distance import Neo4jDistance
 from phi.vectordb.search import SearchType
